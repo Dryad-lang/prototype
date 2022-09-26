cf42ef2571995cb84154ef6426fb079dfe284ca0
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2022-09-26 12:25:32 +0100

    Inicio prototipo v3

---
+++ src/v3/v3/Back/objects/Info.cs
@@ -0,0 +1,8 @@
+namespace v3.back.objects
+{
+    public class Info
+    {
+        public int line { get; set; }
+        public int colum { get; set; }
+    }
+}
---
+++ src/v3/v3/Back/objects/Token.cs
@@ -0,0 +1,13 @@
+namespace v3.back.objects
+{
+    public class Token
+    {
+        public string Char { get; }
+        public string Type { get; }
+
+        public Token()
+        {
+            
+        }
+    }
+}
