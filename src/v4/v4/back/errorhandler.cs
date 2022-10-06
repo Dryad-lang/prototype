a01444560e58e5f1890bf47c154a5f8aaa567831
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2022-10-06 15:11:08 +0100

    algumas coisas

--- src/v4/v4/back/errorhandler.cs
+++ src/v4/v4/back/errorhandler.cs
@@ -13,6 +13,57 @@ public namespace v3.back
         public ErrorType Type { set; get; }
         public string? Message { set; get; }
         public string? Token { set; get; }
-        public Info? Info { set; get; }
+
+    }
+
+    public class ErrorHandler
+    {
+        Console console = new Console();
+
+        Error error = new();
+
+        public ErrorHandler SetType(ErrorType type)
+        {
+            error.Type = type;
+            return this;
+        }
+
+        public ErrorHandler SetMessage(string message)
+        {
+            error.Message = message;
+            return this;
+        }
+
+        public ErrorHandler SetToken(string token)
+        {
+            error.Token = token;
+            return this;
+        }
+
+        public void Throw()
+        {
+            if(error.Type == ErrorType.UnexpectedToken)
+            {
+                console.WriteLine($"Unexpected token '{error.Token}' at line {console.Line} column {console.Column}");
+            }
+            else if(error.Type == ErrorType.SintaxError)
+            {
+                console.WriteLine($"Sintax error at line {console.Line} column {console.Column}");
+            }
+            else if(error.Type == ErrorType.RuntimeError)
+            {
+                console.WriteLine($"Runtime error: {error.Message}");
+            }
+            else if(error.Type == ErrorType.InternalError)
+            {
+                console.WriteLine($"Internal error: {error.Message}");
+            }
+            else
+            {
+                console.WriteLine($"Unknown error: {error.Message}");
+            }
+
+
+        }
     }
 }
