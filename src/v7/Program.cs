88c5e4449be973e75ee72b0d41a108ebd8f0cdae
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2022-12-09 12:54:22 +0000

    changes

---
+++ src/v7/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace v7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
---
+++ src/v7/Tokenizer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace v7
+{
+    public class Token
+    {
+        string? token { get; set; }
+        string? value { get; set; }
+        int pos { get; set; }
+        int line { get; set; }
+        int colum { get; set; }
+    }
+
+    public class Types
+    {
+        Dictionary<string, string> dictionary = new();
+
+        void GetType()
+        {
+        }
+    }
+}
