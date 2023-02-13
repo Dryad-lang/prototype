c4d243e552d0fca54e9a50e1da1f537f6bf07498
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2023-02-13 12:09:38 +0000

    Changes

---
+++ src/v8/v8/backend/lexer/Tokenizer/ITokenizer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace v8.backend.lexer.Tokenizer
+{
+    interface ITokenizer
+    {
+        /*
+        This is the interface for tokenizers
+        
+        Tha language use multiples compilers for do diferent jobs
+        - M -> Math Expressions
+        - P -> Procedures
+        - O -> Objects
+
+        Due to this the intepreter need 3 types of tokenizers and
+        then need to work togueder 
+        */ 
+
+        // This method will see if the imput is on the rules of the tokenizer
+        bool IsMatch(string input);
+    }
+}
---
+++ src/v8/v8/backend/lexer/Tokenizer/P/Rules.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace v8.backend.lexer.Tokenizer.P
+{
+    class Rules
+    {
+    }
+}
