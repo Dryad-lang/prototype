d857c5ed70132a5c169c9da28a3d464421069d23
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2023-03-14 11:47:28 +0000

    Update main.js

--- src/testV11/main.js
+++ src/testV11/main.js
@@ -232,6 +232,31 @@ class ASTNode {
     }
 }
 
+/*
+
+{
+    "type": "program",
+    "value": "",
+    "children": [
+        {
+            "type": "MathExpression",
+            "value": "+",
+            "children": [
+                {
+                    "type": "NumberLiteral",
+                    "value":"2",
+                    "children": []
+                },
+                {
+                    "type": "NumberLiteral",
+                    "value":"3",
+                    "children": []
+                }
+            ]
+        }        
+}
+*/ 
+
 class Parser {
     constructor(lexer) {
         this.lexer = lexer;
@@ -249,4 +274,225 @@ class Parser {
             this.error();
         }
     }
+
+    getNextBody(node, position){
+        return node.body[position];
+    }
+
+    parse(input) {
+        let ast = new AST();
+        ast.root = this.program();
+        return ast;
+    }
+
+    program() {
+        let node = new ASTNode("program", "");
+        while (this.currentToken.type != EOF) {
+            node.children.push(this.statement());
+        }
+        return node;
+    }
+
+    statement() {
+        if (this.currentToken.type == IDENTIFIER) {
+            return this.choiceIdentifyer();
+        } else if (this.currentToken.type == FN) {
+            return this.functionDefinition();
+        } else if (this.currentToken.type == RETURN) {
+            return this.returnStatement();
+        } else if (this.currentToken.type == STRING) {
+            return this.stringLiteral();
+        } else if (this.currentToken.type == NUMBER) {
+            return this.numberLiteral();
+        } else {
+            return this.expressionStatement();
+        }
+    }
+
+    choiceIdentifyer() {
+        // Switch between function calls and assingment statement
+        let type = this.currentToken;
+        this.eat(IDENTIFIER);
+        if (this.currentToken.type == LPAREN) {
+            return this.functionCall(type);
+        }
+        return this.assignmentStatement(type);
+    }
+
+    functionCall(type) {
+        let node = new ASTNode("functionCall", type.value);
+        this.eat(LPAREN);
+        while (this.currentToken.type != RPAREN) {
+            node.children.push(this.expression());
+            if (this.currentToken.type == COMMA) {
+                this.eat(COMMA);
+            }
+        }
+        this.eat(RPAREN);
+        return node;
+    }
+
+    assignmentStatement(type) {
+        /*
+        Types of assingments:
+
+        a = 2; literals
+        a = 1 + 1; expressions
+        a = sum(1, 1); functions call
+        */
+
+        let node = new ASTNode("assignmentStatement", type.value);
+        this.eat(ASSIGN);
+        node.children.push(this.expression());
+        this.eat(SEMICOLON);
+        return node;
+    }
+
+    functionDefinition() {
+        let node = new ASTNode("functionDefinition", "");
+        this.eat(FN);
+        node.children.push(this.identifier());
+        this.eat(LPAREN);
+        node.children.push(this.functionParameters());
+        this.eat(RPAREN);
+        node.children.push(this.blockStatement());
+        return node;
+    }
+
+    functionParameters() {
+        let node = new ASTNode("functionParameters", "");
+        while (this.currentToken.type != RPAREN) {
+            node.children.push(this.identifier());
+            if (this.currentToken.type == COMMA) {
+                this.eat(COMMA);
+            }
+        }
+        return node;
+    }
+
+    returnStatement() {
+        let node = new ASTNode("returnStatement", "");
+        this.eat(RETURN);
+        node.children.push(this.expression());
+        this.eat(SEMICOLON);
+        return node;
+    }
+
+    expressionStatement() {
+        let node = new ASTNode("expressionStatement", "");
+        node.children.push(this.expression());
+        this.eat(SEMICOLON);
+        return node;
+    }
+
+    blockStatement() {
+        let node = new ASTNode("blockStatement", "");
+        this.eat(LBRACE);
+        while (this.currentToken.type != RBRACE) {
+            node.children.push(this.statement());
+        }
+        this.eat(RBRACE);
+        return node;
+    }
+
+    expression() {
+        return this.addition();
+    }
+
+    addition() {
+        let node = this.multiplication();
+        while (this.currentToken.type == PLUS || this.currentToken.type == MINUS) {
+            let token = this.currentToken;
+            if (token.type == PLUS) {
+                this.eat(PLUS);
+            } else if (token.type == MINUS) {
+                this.eat(MINUS);
+            }
+            node = new ASTNode("MathExpression", token.value, [node, this.multiplication()]);
+        }
+        return node;
+    }
+
+    multiplication() {
+        let node = this.factor();
+        while (this.currentToken.type == MULTIPLY || this.currentToken.type == DIVIDE) {
+            let token = this.currentToken;
+            if (token.type == MULTIPLY) {
+                this.eat(MULTIPLY);
+            } else if (token.type == DIVIDE) {
+                this.eat(DIVIDE);
+            }
+            node = new ASTNode("MathExpression", token.value, [node, this.factor()]);
+        }
+        return node;
+    }
+
+    factor() {
+        let token = this.currentToken;
+        if (token.type == PLUS) {
+            this.eat(PLUS);
+            return new ASTNode("MathExpression", token.value, [this.factor()]);
+        } else if (token.type == MINUS) {
+            this.eat(MINUS);
+            return new ASTNode("MathExpression", token.value, [this.factor()]);
+        } else if (token.type == NUMBER) {
+            this.eat(NUMBER);
+            return new ASTNode("NumberLiteral", token.value);
+        } else if (token.type == LPAREN) {
+            this.eat(LPAREN);
+            let node = this.expression();
+            this.eat(RPAREN);
+            return node;
+        } else if (token.type == IDENTIFIER) {
+            return this.identifier();
+        }
+    }
+
+    identifier() {
+        let token = this.currentToken;
+        this.eat(IDENTIFIER);
+        return new ASTNode("Identifier", token.value);
+    }
+
+    stringLiteral() {
+        let token = this.currentToken;
+        this.eat(STRING);
+        return new ASTNode("StringLiteral", token.value);
+    }
+
+    numberLiteral() {
+        let token = this.currentToken;
+        this.eat(NUMBER);
+        return new ASTNode("NumberLiteral", token.value);
+    }
+
+    eat(tokenType) {
+        if (this.currentToken.type == tokenType) {
+            this.currentToken = this.lexer.getNextToken();
+        } else {
+            this.error();
+        }
+    }
+
+    error() {
+        throw "Syntax Error";
+    }
 }
+
+// Test
+
+// let text = `
+// fn sum(a, b) {
+//     return a + b;
+// }
+// `
+
+// let lexer = new Lexer(text);
+
+// let parser = new Parser(lexer);
+
+// let ast = parser.parse();
+
+// console.log(
+//     JSON.stringify(ast, null, 2)
+// );
