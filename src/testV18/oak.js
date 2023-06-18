b57e47db8a57e506e0b40b5063f7ec15b56659d4
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2023-06-18 14:42:57 +0100

    Oak + dryad console interface

---
+++ src/testV18/oak.js
@@ -0,0 +1,34 @@
+/*
+-----------------------------------------------------
+
+OAK Package Manager
+
+-----------------------------------------------------
+
+main file
+
+commands:
+
+    oak init 
+    1- project name
+    2- project description
+    3- project version
+    4- project author
+    5- project license
+    6- project repository
+        - create project/oak.json file
+        - create project/src folder
+        - create project/src/main.dyd file
+        - create project/oak_modules folder
+        - create project/oak_modules/externals folder
+        - create project/oak_modules/oakdata folder
+        
+
+        tree:
+
+
+
+
+*/ 
+
+
